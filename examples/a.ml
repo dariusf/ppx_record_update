6369ca26585873ed0f74eb8e10d7c52a34bf8472
Darius Foo <darius.foo.tw@gmail.com>
2022-01-08 12:25:40 +0800

    Update for ocamlformat's deprecations

--- examples/a.ml
+++ examples/a.ml
@@ -1,19 +1,25 @@
-
 module Person = struct
-  type t = { id : int; name : string }
+  type t = {
+    id : int;
+    name : string;
+  }
   [@@deriving show]
 end
+
 module Book = struct
-  type t = { borrowed_by : Person.t; title : string }
+  type t = {
+    borrowed_by : Person.t;
+    title : string;
+  }
   [@@deriving show]
 end
 
 let _ =
-
   let p = { Person.id = 10; name = "bob" } in
   let b = Book.{ borrowed_by = p; title = "anna karenina" } in
 
-  print_endline @@ Book.show [%record
-    b.borrowed_by.name <- "lol";
-    b.borrowed_by.id <~ fun x -> x + 1;
-  ]
+  print_endline
+  @@ Book.show
+       [%record
+         b.borrowed_by.name <- "lol";
+         b.borrowed_by.id <~ fun x -> x + 1]
